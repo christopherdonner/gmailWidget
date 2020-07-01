1c5f31483cddef733b5709a87d0ba37e66b7453c
Christopher Donner <cdonner@opentext.com>
2020-06-30 22:24:59 -0400

    subject, from, date and snippet being surfaced

--- public/main.js
+++ public/main.js
@@ -1,35 +1,57 @@
-var msg={}  
+var msg={
+  date: '',
+  subject: '',
+  from: ''
+}  
 
 
-function getDate(name){
-return name =="date"
+function getDate(value){
+if(name==="date"){
+   return value
+}
+}
+
+function getSubject(value){
+  if(name==="Subject"){
+    console.log(value)
+    return value
+  }
+}
+
+function getFrom(value){
+  if(this.name==="From"){
+    return value
+  }
 }
 
 $.getJSON("/messages", function (data) {
   console.log(data)
     // console.log(data[0].data)
     data.forEach((message)=>{
-      console.log(message)
+      console.log(message.data)
       let header=message.data.payload.headers
-      console.log(header)
       header.forEach((prop)=>{
         console.log(prop)
-        if(prop.name=='Date'){
+        if(prop.name=="Subject"){
+          msg.subject=prop.value
+        }
+        if(prop.name=="From"){
+          msg.from=prop.value
+        }
+        if(prop.name=="Date"){
           msg.date=prop.value
         }
-        // if(prop.name=='Subject'){
-        //   msg.subject=prop.subject
-        // }
       })
       console.log(msg)
       $(".messages").append(`
-      <button type="button" class="btn btn-primary">
+      <button type="button" class="btn btn-primary" title='${message.data.snippet}'>
       <div class="container">
-      <p>${msg.date}</p>
-      <h3>${msg.subject}</h3>
+      <div class="row"><div class="col-6">${msg.date}</div><div class="col-4 offset-2">From: ${msg.from}</div></div>
+      <div class="row"></div>
+      <div class="row"><div class="col-10 offset-1"><h4>${msg.subject}</h4></div></div>
       </div>
       </button>
-      <br>`)
+      <br><br>`)
     })
   });
   
